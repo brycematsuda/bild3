cb74d05b8e84b4ced2393a0085a77747dd5cd055
Bryce Matsuda <iexist4jcore@gmail.com>
2014-07-27 14:05:42 -1000

    Remove old CRUD table schema

--- schema.sql
+++ schema.sql
@@ -1,18 +1,4 @@
-drop table if exists entries;
 drop table if exists billboard100;
-create table entries (
-  id integer primary key autoincrement,
-  title varchar(255) not null,
-  artist varchar(255),
-  album varchar(255),
-  new_title varchar(255),
-  new_artist varchar(255),
-  new_album varchar(255),
-  quality varchar(100),
-  edits varchar(255),
-  update_status varchar(100) not null,
-  notes text
-);
 
 create table billboard100(
   rank integer primary key not null,
